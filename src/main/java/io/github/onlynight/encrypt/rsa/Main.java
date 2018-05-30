2ad6e57195888b7884d0d61b31f5123d07627042
wyndam <only.night@qq.com>
2018-05-30 16:11:42 +0800

    1. delete jd-gui comment 2. add load encrypt key from inputstream

--- src/main/java/io/github/onlynight/encrypt/rsa/Main.java
+++ src/main/java/io/github/onlynight/encrypt/rsa/Main.java
@@ -1,4 +1,3 @@
-
 package io.github.onlynight.encrypt.rsa;
 
 import org.bouncycastle.util.encoders.Base64;
@@ -20,7 +19,6 @@ public class Main {
         System.out.println("加密：" + new String(cipher));
         System.out.println("解密：" + restr);
         System.out.println();
-
         System.out.println("--------------私钥加密公钥解密过程-------------------");
         plainText = "ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密ihep_公钥加密私钥解密";
         cipherData = RSAEncrypt.encrypt(RSAEncrypt.loadPrivateKeyByStr(RSAEncrypt.loadPrivateKeyByFile(filepath)), plainText.getBytes());
@@ -31,14 +29,12 @@ public class Main {
         System.out.println("加密：" + new String(cipher));
         System.out.println("解密：" + restr);
         System.out.println();
-
         System.out.println("---------------私钥签名过程------------------");
         String content = "ihep_这是用于签名的原始数据";
         String signstr = RSASignature.sign(content, RSAEncrypt.loadPrivateKeyByFile(filepath));
         System.out.println("签名原串：" + content);
         System.out.println("签名串：" + signstr);
         System.out.println();
-
         System.out.println("---------------公钥校验签名------------------");
         System.out.println("签名原串：" + content);
         System.out.println("签名串：" + signstr);
--- src/main/java/io/github/onlynight/encrypt/rsa/RSAEncrypt.java
+++ src/main/java/io/github/onlynight/encrypt/rsa/RSAEncrypt.java
@@ -21,17 +21,13 @@ public class RSAEncrypt {
     public static final String PRIVATE_KEY_NAME = "rsa_private_key.pem";
     public static final String PUBLIC_KEY_NAME = "rsa_public_key.pem";
 
-
     public static void genKeyPair(String filePath) {
         genKeyPair(filePath, 1024);
     }
 
-
     public static void genKeyPair(String filePath, int keySize) {
         KeyPairGenerator keyPairGen = null;
-
         try {
-
             keyPairGen = KeyPairGenerator.getInstance("RSA");
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
@@ -73,502 +69,267 @@ public class RSAEncrypt {
         }
     }
 
-    public static String loadPrivateKeyByFile(String path)
-            throws Exception {
-        /* 107:    */
-        try
-            /* 108:    */ {
-            /* 109:120 */
+    public static String loadPrivateKeyByFile(String path) throws Exception {
+        try {
+
             return getKeyContent(path, "rsa_private_key.pem");
-            /* 110:    */
-        }
-        /* 111:    */ catch (IOException e)
-            /* 112:    */ {
-            /* 113:122 */
+        } catch (IOException e) {
+
             throw new Exception("私钥数据读取错误");
-            /* 114:    */
-        }
-        /* 115:    */ catch (NullPointerException e)
-            /* 116:    */ {
-            /* 117:124 */
+        } catch (NullPointerException e) {
+
             throw new Exception("私钥输入流为空");
-            /* 118:    */
         }
-        /* 119:    */
     }
 
-    /* 120:    */
-    /* 121:    */
-    public static RSAPublicKey loadPublicKeyByStr(String publicKeyStr)
-    /* 122:    */     throws Exception
-    /* 123:    */ {
-        /* 124:    */
-        try
-            /* 125:    */ {
-            /* 126:137 */
+    public static RSAPublicKey loadPublicKeyByStr(String publicKeyStr) throws Exception {
+
+        try {
+
             byte[] buffer = Base64.decode(publicKeyStr);
-            /* 127:138 */
             KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-            /* 128:139 */
             X509EncodedKeySpec keySpec = new X509EncodedKeySpec(buffer);
-            /* 129:140 */
             return (RSAPublicKey) keyFactory.generatePublic(keySpec);
-            /* 130:    */
-        }
-        /* 131:    */ catch (NoSuchAlgorithmException e)
-            /* 132:    */ {
-            /* 133:142 */
+        } catch (NoSuchAlgorithmException e) {
+
             throw new Exception("无此算法");
-            /* 134:    */
-        }
-        /* 135:    */ catch (InvalidKeySpecException e)
-            /* 136:    */ {
-            /* 137:144 */
+        } catch (InvalidKeySpecException e) {
+
             throw new Exception("公钥非法");
-            /* 138:    */
-        }
-        /* 139:    */ catch (NullPointerException e)
-            /* 140:    */ {
-            /* 141:146 */
+        } catch (NullPointerException e) {
+
             throw new Exception("公钥数据为空");
-            /* 142:    */
         }
-        /* 143:    */
     }
 
-    /* 144:    */
-    /* 145:    */
-    private static String getKeyContent(String path, String keyName)
-    /* 146:    */     throws IOException
-    /* 147:    */ {
-        /* 148:151 */
+    private static String getKeyContent(String path, String keyName) throws IOException {
+
         return loadEncryptKey(new File(path + File.separator + keyName));
-        /* 149:    */
     }
 
-    /* 150:    */
-    /* 151:    */
-    public static String loadEncryptKey(File encryptFile)
-    /* 152:    */     throws IOException
-    /* 153:    */ {
-        /* 154:162 */
+    public static String loadEncryptKey(File encryptFile) throws IOException {
+
         BufferedReader br = new BufferedReader(new FileReader(encryptFile));
-        /* 155:163 */
         String readLine = null;
-        /* 156:164 */
         StringBuilder sb = new StringBuilder();
-        /* 157:165 */
         while ((readLine = br.readLine()) != null) {
-            /* 158:166 */
             sb.append(readLine);
-            /* 159:    */
         }
-        /* 160:168 */
         br.close();
-        /* 161:169 */
         return sb.toString();
-        /* 162:    */
     }
 
-    /* 163:    */
-    /* 164:    */
-    public static RSAPrivateKey loadPrivateKeyByStr(String privateKeyStr)
-    /* 165:    */     throws Exception
-    /* 166:    */ {
-        /* 167:    */
-        try
-            /* 168:    */ {
-            /* 169:175 */
+    public static String loadEncryptKey(InputStream inputStream) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+        String readLine = null;
+        StringBuilder sb = new StringBuilder();
+        while ((readLine = br.readLine()) != null) {
+            sb.append(readLine);
+        }
+        br.close();
+        return sb.toString();
+    }
+
+    public static RSAPrivateKey loadPrivateKeyByStr(String privateKeyStr) throws Exception {
+
+        try {
+
             byte[] buffer = Base64.decode(privateKeyStr);
-            /* 170:176 */
             PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(buffer);
-            /* 171:177 */
             KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-            /* 172:178 */
             return (RSAPrivateKey) keyFactory.generatePrivate(keySpec);
-            /* 173:    */
-        }
-        /* 174:    */ catch (NoSuchAlgorithmException e)
-            /* 175:    */ {
-            /* 176:180 */
+        } catch (NoSuchAlgorithmException e) {
+
             throw new Exception("无此算法");
-            /* 177:    */
-        }
-        /* 178:    */ catch (InvalidKeySpecException e)
-            /* 179:    */ {
-            /* 180:182 */
+        } catch (InvalidKeySpecException e) {
+
             throw new Exception("私钥非法");
-            /* 181:    */
-        }
-        /* 182:    */ catch (NullPointerException e)
-            /* 183:    */ {
-            /* 184:184 */
+        } catch (NullPointerException e) {
+
             throw new Exception("私钥数据为空");
-            /* 185:    */
         }
-        /* 186:    */
     }
 
-    /* 187:    */
-    /* 188:    */
-    public static byte[] encrypt(RSAPublicKey publicKey, byte[] plainTextData)
-    /* 189:    */     throws Exception
-    /* 190:    */ {
-        /* 191:198 */
+    public static byte[] encrypt(RSAPublicKey publicKey, byte[] plainTextData) throws Exception {
+
         if (publicKey == null) {
-            /* 192:199 */
             throw new Exception("加密公钥为空, 请设置");
-            /* 193:    */
         }
-        /* 194:201 */
         Cipher cipher = null;
-        /* 195:    */
-        try
-            /* 196:    */ {
-            /* 197:204 */
+        try {
+
             cipher = Cipher.getInstance("RSA");
-            /* 198:    */
-            /* 199:206 */
+
             cipher.init(1, publicKey);
-            /* 200:207 */
             return innerCipher(cipher, plainTextData, 117);
-            /* 201:    */
-        }
-        /* 202:    */ catch (NoSuchAlgorithmException e)
-            /* 203:    */ {
-            /* 204:209 */
+        } catch (NoSuchAlgorithmException e) {
+
             throw new Exception("无此加密算法");
-            /* 205:    */
-        }
-        /* 206:    */ catch (NoSuchPaddingException e)
-            /* 207:    */ {
-            /* 208:211 */
+        } catch (NoSuchPaddingException e) {
+
             e.printStackTrace();
-            /* 209:212 */
             return null;
-            /* 210:    */
-        }
-        /* 211:    */ catch (InvalidKeyException e)
-            /* 212:    */ {
-            /* 213:214 */
+        } catch (InvalidKeyException e) {
+
             throw new Exception("加密公钥非法,请检查");
-            /* 214:    */
-        }
-        /* 215:    */ catch (IllegalBlockSizeException e)
-            /* 216:    */ {
-            /* 217:216 */
+        } catch (IllegalBlockSizeException e) {
+
             throw new Exception("明文长度非法");
-            /* 218:    */
-        }
-        /* 219:    */ catch (BadPaddingException e)
-            /* 220:    */ {
-            /* 221:218 */
+        } catch (BadPaddingException e) {
+
             throw new Exception("明文数据已损坏");
-            /* 222:    */
         }
-        /* 223:    */
     }
 
-    /* 224:    */
-    /* 225:    */
-    public static byte[] encrypt(RSAPrivateKey privateKey, byte[] plainTextData)
-    /* 226:    */     throws Exception
-    /* 227:    */ {
-        /* 228:232 */
+    public static byte[] encrypt(RSAPrivateKey privateKey, byte[] plainTextData) throws Exception {
+
         if (privateKey == null) {
-            /* 229:233 */
             throw new Exception("加密私钥为空, 请设置");
-            /* 230:    */
         }
-        /* 231:235 */
         Cipher cipher = null;
-        /* 232:    */
-        try
-            /* 233:    */ {
-            /* 234:238 */
+        try {
+
             cipher = Cipher.getInstance("RSA");
-            /* 235:239 */
             cipher.init(1, privateKey);
-            /* 236:240 */
             return innerCipher(cipher, plainTextData, 117);
-            /* 237:    */
-        }
-        /* 238:    */ catch (NoSuchAlgorithmException e)
-            /* 239:    */ {
-            /* 240:242 */
+        } catch (NoSuchAlgorithmException e) {
+
             throw new Exception("无此加密算法");
-            /* 241:    */
-        }
-        /* 242:    */ catch (NoSuchPaddingException e)
-            /* 243:    */ {
-            /* 244:244 */
+        } catch (NoSuchPaddingException e) {
+
             e.printStackTrace();
-            /* 245:245 */
             return null;
-            /* 246:    */
-        }
-        /* 247:    */ catch (InvalidKeyException e)
-            /* 248:    */ {
-            /* 249:247 */
+        } catch (InvalidKeyException e) {
+
             throw new Exception("加密私钥非法,请检查");
-            /* 250:    */
-        }
-        /* 251:    */ catch (IllegalBlockSizeException e)
-            /* 252:    */ {
-            /* 253:249 */
+        } catch (IllegalBlockSizeException e) {
+
             throw new Exception("明文长度非法");
-            /* 254:    */
-        }
-        /* 255:    */ catch (BadPaddingException e)
-            /* 256:    */ {
-            /* 257:251 */
+        } catch (BadPaddingException e) {
+
             throw new Exception("明文数据已损坏");
-            /* 258:    */
         }
-        /* 259:    */
     }
 
-    /* 260:    */
-    /* 261:    */
-    private static byte[] innerCipher(Cipher cipher, byte[] plainTextData, int maxLength)
-    /* 262:    */     throws BadPaddingException, IllegalBlockSizeException, IOException
-    /* 263:    */ {
-        /* 264:256 */
+    private static byte[] innerCipher(Cipher cipher, byte[] plainTextData, int maxLength) throws BadPaddingException, IllegalBlockSizeException, IOException {
+
         int inputLen = plainTextData.length;
-        /* 265:257 */
         ByteArrayOutputStream out = new ByteArrayOutputStream();
-        /* 266:258 */
         int offSet = 0;
-        /* 267:    */
-        /* 268:260 */
+
         int i = 0;
-        /* 269:261 */
-        while (inputLen - offSet > 0)
-            /* 270:    */ {
-            /* 271:    */
+        while (inputLen - offSet > 0) {
+
             byte[] cache;
-            /* 273:262 */
             if (inputLen - offSet > maxLength) {
-                /* 274:263 */
                 cache = cipher.doFinal(plainTextData, offSet, maxLength);
-                /* 275:    */
             } else {
-                /* 276:265 */
                 cache = cipher.doFinal(plainTextData, offSet, inputLen - offSet);
-                /* 277:    */
             }
-            /* 278:267 */
             out.write(cache, 0, cache.length);
-            /* 279:268 */
             i++;
-            /* 280:269 */
             offSet = i * maxLength;
-            /* 281:    */
         }
-        /* 282:271 */
         byte[] decryptedData = out.toByteArray();
-        /* 283:272 */
         out.close();
-        /* 284:273 */
         return decryptedData;
-        /* 285:    */
     }
 
-    /* 286:    */
-    /* 287:    */
-    public static byte[] encrypt(RSAPublicKey publicKey, String data)
-    /* 288:    */     throws Exception
-    /* 289:    */ {
-        /* 290:286 */
+    public static byte[] encrypt(RSAPublicKey publicKey, String data) throws Exception {
+
         return encrypt(publicKey, data.getBytes());
-        /* 291:    */
     }
 
-    /* 292:    */
-    /* 293:    */
-    public static byte[] encrypt(RSAPrivateKey privateKey, String data)
-    /* 294:    */     throws Exception
-    /* 295:    */ {
-        /* 296:298 */
+    public static byte[] encrypt(RSAPrivateKey privateKey, String data) throws Exception {
+
         return encrypt(privateKey, data.getBytes());
-        /* 297:    */
     }
 
-    /* 298:    */
-    /* 299:    */
-    public static byte[] decrypt(RSAPrivateKey privateKey, byte[] cipherData)
-    /* 300:    */     throws Exception
-    /* 301:    */ {
-        /* 302:311 */
+    public static byte[] decrypt(RSAPrivateKey privateKey, byte[] cipherData) throws Exception {
+
         if (privateKey == null) {
-            /* 303:312 */
             throw new Exception("解密私钥为空, 请设置");
-            /* 304:    */
         }
-        /* 305:314 */
         Cipher cipher = null;
-        /* 306:    */
-        try
-            /* 307:    */ {
-            /* 308:317 */
+        try {
+
             cipher = Cipher.getInstance("RSA");
-            /* 309:    */
-            /* 310:319 */
+
             cipher.init(2, privateKey);
-            /* 311:320 */
             return innerCipher(cipher, cipherData, 128);
-            /* 312:    */
-        }
-        /* 313:    */ catch (NoSuchAlgorithmException e)
-            /* 314:    */ {
-            /* 315:322 */
+        } catch (NoSuchAlgorithmException e) {
+
             throw new Exception("无此解密算法");
-            /* 316:    */
-        }
-        /* 317:    */ catch (NoSuchPaddingException e)
-            /* 318:    */ {
-            /* 319:324 */
+        } catch (NoSuchPaddingException e) {
+
             e.printStackTrace();
-            /* 320:325 */
             return null;
-            /* 321:    */
-        }
-        /* 322:    */ catch (InvalidKeyException e)
-            /* 323:    */ {
-            /* 324:327 */
+        } catch (InvalidKeyException e) {
+
             throw new Exception("解密私钥非法,请检查");
-            /* 325:    */
-        }
-        /* 326:    */ catch (IllegalBlockSizeException e)
-            /* 327:    */ {
-            /* 328:329 */
+        } catch (IllegalBlockSizeException e) {
+
             throw new Exception("密文长度非法");
-            /* 329:    */
-        }
-        /* 330:    */ catch (BadPaddingException e)
-            /* 331:    */ {
-            /* 332:331 */
+        } catch (BadPaddingException e) {
+
             throw new Exception("密文数据已损坏");
-            /* 333:    */
         }
-        /* 334:    */
     }
 
-    /* 335:    */
-    /* 336:    */
-    public static byte[] decrypt(RSAPublicKey publicKey, byte[] cipherData)
-    /* 337:    */     throws Exception
-    /* 338:    */ {
-        /* 339:345 */
+    public static byte[] decrypt(RSAPublicKey publicKey, byte[] cipherData) throws Exception {
+
         if (publicKey == null) {
-            /* 340:346 */
             throw new Exception("解密公钥为空, 请设置");
-            /* 341:    */
         }
-        /* 342:348 */
         Cipher cipher = null;
-        /* 343:    */
-        try
-            /* 344:    */ {
-            /* 345:351 */
+        try {
+
             cipher = Cipher.getInstance("RSA");
-            /* 346:    */
-            /* 347:353 */
+
             cipher.init(2, publicKey);
-            /* 348:354 */
             return innerCipher(cipher, cipherData, 128);
-            /* 349:    */
-        }
-        /* 350:    */ catch (NoSuchAlgorithmException e)
-            /* 351:    */ {
-            /* 352:356 */
+        } catch (NoSuchAlgorithmException e) {
+
             throw new Exception("无此解密算法");
-            /* 353:    */
-        }
-        /* 354:    */ catch (NoSuchPaddingException e)
-            /* 355:    */ {
-            /* 356:358 */
+        } catch (NoSuchPaddingException e) {
+
             e.printStackTrace();
-            /* 357:359 */
             return null;
-            /* 358:    */
-        }
-        /* 359:    */ catch (InvalidKeyException e)
-            /* 360:    */ {
-            /* 361:361 */
+        } catch (InvalidKeyException e) {
+
             throw new Exception("解密公钥非法,请检查");
-            /* 362:    */
-        }
-        /* 363:    */ catch (IllegalBlockSizeException e)
-            /* 364:    */ {
-            /* 365:363 */
+        } catch (IllegalBlockSizeException e) {
+
             throw new Exception("密文长度非法");
-            /* 366:    */
-        }
-        /* 367:    */ catch (BadPaddingException e)
-            /* 368:    */ {
-            /* 369:365 */
+        } catch (BadPaddingException e) {
+
             throw new Exception("密文数据已损坏");
-            /* 370:    */
         }
-        /* 371:    */
     }
 
-    /* 372:    */
-    /* 373:    */
-    public static byte[] decrypt(RSAPrivateKey privateKey, String data)
-    /* 374:    */     throws Exception
-    /* 375:    */ {
-        /* 376:379 */
+    public static byte[] decrypt(RSAPrivateKey privateKey, String data) throws Exception {
+
         return decrypt(privateKey, data.getBytes());
-        /* 377:    */
     }
 
-    /* 378:    */
-    /* 379:    */
-    public static byte[] decrypt(RSAPublicKey publicKey, String data)
-    /* 380:    */     throws Exception
-    /* 381:    */ {
-        /* 382:392 */
+    public static byte[] decrypt(RSAPublicKey publicKey, String data) throws Exception {
+
         return decrypt(publicKey, data.getBytes());
-        /* 383:    */
     }
 
-    /* 384:    */
-    /* 385:    */
-    public static String byteArrayToString(byte[] data)
-    /* 386:    */ {
-        /* 387:402 */
+    public static String byteArrayToString(byte[] data) {
+
         StringBuilder stringBuilder = new StringBuilder();
-        /* 388:403 */
-        for (int i = 0; i < data.length; i++)
-            /* 389:    */ {
-            /* 390:405 */
+        for (int i = 0; i < data.length; i++) {
+
             stringBuilder.append(HEX_CHAR[((data[i] & 0xF0) >>> 4)]);
-            /* 391:    */
-            /* 392:407 */
+
             stringBuilder.append(HEX_CHAR[(data[i] & 0xF)]);
-            /* 393:408 */
             if (i < data.length - 1) {
-                /* 394:409 */
                 stringBuilder.append(' ');
-                /* 395:    */
             }
-            /* 396:    */
         }
-        /* 397:412 */
         return stringBuilder.toString();
-        /* 398:    */
     }
-    /* 399:    */
 }
-
-
-
-/* Location:           C:\Users\zhangwenda\Desktop\rsa-encrypt-1.0-SNAPSHOT.jar
-
- * Qualified Name:     io.github.onlynight.encrypt.rsa.RSAEncrypt
-
- * JD-Core Version:    0.7.0.1
-
- */
